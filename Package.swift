eb8b30df5cf2af69df8659c760f297a58a392e8f
Mark Watson <markw@markwatson.com>
2025-11-23 10:24:26 -0700

    -

--- Package.swift
+++ Package.swift
@@ -2,29 +2,37 @@
 import PackageDescription
 
 let package = Package(
-    name: "Anthropic_swift_examples",
+    name: "MLX_swift_examples",
     platforms: [
         .macOS("14.0")
     ],
     products: [
         .library(
-            name: "Anthropic_swift_examples",
-            targets: ["Anthropic_swift_examples"]
+            name: "MLX_swift_examples",
+            targets: ["MLX_swift_examples"]
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/fumito-ito/AnthropicSwiftSDK.git", from: "0.8.0")
+        .package(url: "https://github.com/ml-explore/mlx-swift", from: "0.18.1"),
+        .package(url: "https://github.com/ml-explore/mlx-swift-examples", branch: "main")
     ],
     targets: [
         .target(
-            name: "Anthropic_swift_examples",
+            name: "MLX_swift_examples",
             dependencies: [
-                .product(name: "AnthropicSwiftSDK", package: "AnthropicSwiftSDK")
+                .product(name: "MLX", package: "mlx-swift"),
+                .product(name: "LLM", package: "mlx-swift-examples")
             ]
         ),
         .testTarget(
-            name: "Anthropic_swift_examplesTests",
-            dependencies: ["Anthropic_swift_examples"]
+            name: "MLX_swift_examplesTests",
+            dependencies: [
+               .product(name: "MLX", package: "mlx-swift"),
+               .product(name: "MLXRandom", package: "mlx-swift"),
+               .product(name: "MLXNN", package: "mlx-swift"),
+               .product(name: "MLXOptimizers", package: "mlx-swift"),
+               .product(name: "MLXFFT", package: "mlx-swift")
+            ]
         ),
     ]
 )
